d04e8174545e2a575cec1f16159f60f242a78f62
Erick Alves <britoerick16@gmail.com>
2025-11-29 10:49:27 -0300

    fix: correção no retorno de todos os dados de uma entidade

--- Interfaces/IClientController.cs
+++ Interfaces/IClientController.cs
@@ -6,7 +6,7 @@ namespace TekShop.Interface
     {
         public Task Add(T entity);
         public Task<T?> GetById(int id);
-        public List<T>? GetAll();
+        public Task<List<T>>? GetAll();
         public Task Update(T entity);
         public Task Delete(T entity);
         public Task Delete(int id);
